fa91615c22373a2c1364a4277f371d660e191286
kmartinez <km@ecs.soton.ac.uk>
2017-01-13 10:42:55 +0000

    Modified script as the extra error doesn't exist any more. Modified comments at top to remove extra bin from path

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # this is the setup file - run once only to make the package:
 # python setup.py sdist
 # will make the tarball. This can be unpacked then:
-# python setup.py install --prefix=/usr/local/bin
+# python setup.py install --prefix=/usr/local
 
 from distutils.core import setup, Extension
 
@@ -14,7 +14,7 @@ setup(name='RTIAcquire',
     license='LICENSE.txt',
     description='Remote-control of digital cameras', 
     ext_modules=[Extension('rtiacquire.dejpeg', ['rtiacquire/dejpeg.c'], 
-        libraries=['jpeg'], extra_compile_args=['-Wno-error=unused-command-line-argument-hard-error-in-future'])],
+        libraries=['jpeg'])],
     package_data={'rtiacquire': ['data/*']},
     requires=['pyserial'],
     long_description=open('README.md').read(),
